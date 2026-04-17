8ca7abb9a782f9bf96f12bda152c41d2cc0e9576
dharanika27 <dharanika27112003@gmail.com>
2026-04-17 19:36:06 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -22,26 +22,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train) # Train model
+            para=param[list(models.keys())[i]]
 
-            # Predict Testing data
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
-            
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-            # Get R2 scores for train and test data
+
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
